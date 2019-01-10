3ef16be4c4811566e83a100955938e699ed6846e
Daniel Hernandez <danhr90544@gmail.com>
2019-01-10 23:06:08 +0000

    Playlist

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ["Gang Plank Galleon","Blizzard","Clint Eastwood","DB改MAD】ヒカリへのカウントダウン","Sunflower","Flying Battery Zone 1","Lucky You"];
 // BELOW Add 4 More arrays to store images_links, atists, song lengths, and links for each song
 // Make sure they match the same order as your array above
-
-
-
+var images=[];
+var artists=[];
+var lengths=[];
+var links=["https://www.youtube.com/watch?v=2oT4bnjlDNE","https://www.youtube.com/watch?v=-mbF5s1W4VQ","","","","",""];
 
     // BELOW Use forEach Loop to display the data from each of your array's in the correct div
 
